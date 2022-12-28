b52b0d70f50fc0228d3f066587fac90350e26d5e
0520 <72007390+jizhi0520@users.noreply.github.com>
2022-12-28 08:29:15 +0800

    Add files via upload

---
+++ cubox.js
@@ -0,0 +1,41 @@
+/******************************
+
+脚本功能：cubox-收藏阅读+解锁VIP
+下载地址：http://mtw.so/5IpOxH
+软件版本：7.1.1
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/cubox.js
+
+[mitm] 
+
+hostname = cubox.pro
+
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/userInfo';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.data.level = 1;
+    obj.data.expireTime = "2999-09-09T23:50:23+08:00";
+    obj.data.nickName = "且以清欢度流年";
+    obj.data.thirdNickName = "且以清欢度流年";
+    obj.data.isExpire = false;
+    obj.data.active = true;
+    obj.data.payTime = 1660006006;
+
+	body = JSON.stringify(obj);
+}
+
+
+$done({body});
