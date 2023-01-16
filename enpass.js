eff8d9590eba5e9d975915e07f0e26208ff0383c
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-01-17 07:22:44 +0800

    Update enpass.js

--- enpass.js
+++ enpass.js
@@ -1,11 +1,3 @@
-[rewrite_local]
-# Enpass Password Manager解锁订阅
-^https?:\/\/license\.enpass\.io\/api\/v1\/subscription\/me  url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/enpass.js
-
-[mitm]
-hostname = license.enpass.io
-*/
-
 var obj = JSON.parse($response.body);
 
 obj.license = "premium";
