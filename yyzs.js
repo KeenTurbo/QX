dda1b5d43716505d0ed209dcb34417986cde0075
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-02-25 15:59:14 +0800

    Update yyzs.js

--- yyzs.js
+++ yyzs.js
@@ -18,41 +18,86 @@ hostname=newdrugs.dxy.cn
 
 *******************************/
 
-var body = $response['body']['replace'](/false/g, 'true');
-var url = $request['url'];
-var obj = JSON['parse'](body);
-var sj = '2099-07-30 23:50:35';
-var bz = '9999999999999';
-var qm = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg10.360buyimg.com%2Fn0%2Fjfs%2Ft1%2F66253%2F38%2F12315%2F158714%2F5d9caed4Ef32b47b9%2F33d8d07a8489788a.jpg&refer=http%3A%2F%2Fimg10.360buyimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1661201310&t=651564aec8b189f326a483d97de6af5b';
-const vip = '/app/user/init';
-const viq = '/app/user/pro/stat';
-const mz = '/app/user';
-if (url['indexOf'](mz) != -1) {
-    obj['data']['avatar'] = qm;
+var __encode = 'baidu.com',
+    _a = {},
+    _0xb483 = ["_decode", "http://www.sojson.com/javascriptobfuscator.html"];
+(function(_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(_a);
+var __Oxe6762 = ["ture", "replace", "body", "url", "parse", "2099-07-30 23:50:35;", "9999999999999", "瞅啥瞅", "https://cdn.dribbble.com/users/3972215/screenshots/14522042/media/7a72b7b1ab3c9445e7a0795e1be2e3c7.jpg", "/app/user/init", "/app/user/pro/stat", "/app/user", "indexOf", "username", "data", "nickname", "avatar", "stringify", "expiredDay", "newMessage", "isProActive", "subscribe", "userProStatStatusEnum", "VALID", "expireDate", "expiredTime", "userProInfoVO", "subscribeExpiresDate", "placeholderVip", "false", "activeType", "svipUserProInfo", "log", "undefined", "删除", "版本号，js会定", "期弹窗，", "还请支持我们的工作", "jsjia", "mi.com"];
+var body = $response[__Oxe6762[0x2]][__Oxe6762[0x1]](/false/g, __Oxe6762[0x0]);
+var body = $response[__Oxe6762[0x2]];
+var url = $request[__Oxe6762[0x3]];
+var obj = JSON[__Oxe6762[0x4]](body);
+var sj = __Oxe6762[0x5];
+var bz = __Oxe6762[0x6];
+var ll = __Oxe6762[0x7];
+var qm = __Oxe6762[0x8];
+const vip = __Oxe6762[0x9];
+const viq = __Oxe6762[0xa];
+const mz = __Oxe6762[0xb];
+if (url[__Oxe6762[0xc]](mz) != -1) {
+    /*obj[__Oxe6762[0xe]][__Oxe6762[0xd]] = ll;
+    obj[__Oxe6762[0xe]][__Oxe6762[0xf]] = ll;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x10]] = qm;*/
+    body = JSON[__Oxe6762[0x11]](obj)
 };
-if (url['indexOf'](vip) != -1) {
-    obj['data']['expiredDay'] = 999999;
-    obj['data']['newMessage'] = true;
-    obj['data']['isProActive'] = true;
-    obj['data']['subscribe'] = true;
-    obj['data']['userProStatStatusEnum'] = 'VALID';
-    obj['data']['expireDate'] = sj;
-    obj['data']['expiredTime'] = sj;
-    obj['data']['userProInfoVO']['expireDate'] = sj;
-    obj['data']['userProInfoVO']['expiredDay'] = 999;
-    obj['data']['userProInfoVO']['subscribeExpiresDate'] = sj;
-    obj['data']['placeholderVip'] = true;
-    obj['data']['activeType'] = 'svipUserProInfo';
-    obj['data']['log'] = undefined;
-    obj['data']['delete'] = '删除';
+if (url[__Oxe6762[0xc]](vip) != -1) {
+    obj[__Oxe6762[0xe]][__Oxe6762[0x12]] = 999999;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x13]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x14]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x15]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x16]] = __Oxe6762[0x17];
+    obj[__Oxe6762[0xe]][__Oxe6762[0x18]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x19]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x19]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x12]] = 999;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x1b]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x1c]] = true;
+    obj[__Oxe6762[0x1d]] = true;
+    body = JSON[__Oxe6762[0x11]](obj)
 };
-if (url['indexOf'](viq) != -1) {
-    obj['data']['expireDate'] = sj;
-    obj['data']['activeType'] = 'svipUserProInfo';
-    obj['data']['userProInfoVO']['status'] = 3;
-    obj['data']['userProInfoVO']['subscribeExpiresDate'] = sj;
-    obj['data']['userProInfoVO']['subscribeType'] = 'svip';
-    obj['data']['userProInfoVO']['autoRenewal'] = true;
-    obj['newMessage'] = true;
-};
-$done({body: JSON.stringify(obj)});
+if (url[__Oxe6762[0xc]](viq) != -1) {
+    obj[__Oxe6762[0xe]][__Oxe6762[0x16]] = __Oxe6762[0x17];
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1e]] = 3;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x1c]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x12]] = 999999;
+    obj[__Oxe6762[0x1d]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x18]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x19]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x19]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x12]] = bz;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1f]][__Oxe6762[0x12]] = bz;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x1b]] = sj;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1a]][__Oxe6762[0x1e]] = 2;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1f]][__Oxe6762[0x1c]] = true;
+    obj[__Oxe6762[0xe]][__Oxe6762[0x1f]][__Oxe6762[0x1e]] = 2;
+    body = JSON[__Oxe6762[0x11]](obj)
+};/*
+console[__Oxe6762[0x20]](body);*/
+$done({
+    body
+});;;
+(function(_0x4c83xb, _0x4c83xc, _0x4c83xd, _0x4c83xe, _0x4c83xf, _0x4c83x10) {
+    _0x4c83x10 = __Oxe6762[0x21];
+    _0x4c83xe = function(_0x4c83x11) {
+        if (typeof alert !== _0x4c83x10) {
+            alert(_0x4c83x11)
+        };/*
+        if (typeof console !== _0x4c83x10) {
+            console[__Oxe6762[0x20]](_0x4c83x11)
+        }*/
+    };
+    _0x4c83xd = function(_0x4c83x12, _0x4c83xb) {
+        return _0x4c83x12 + _0x4c83xb
+    };
+    _0x4c83xf = _0x4c83xd(__Oxe6762[0x22], _0x4c83xd(_0x4c83xd(__Oxe6762[0x23], __Oxe6762[0x24]), __Oxe6762[0x25]));
+    try {
+        _0x4c83xb = __encode;
+        if (!(typeof _0x4c83xb !== _0x4c83x10 && _0x4c83xb === _0x4c83xd(__Oxe6762[0x26], __Oxe6762[0x27]))) {
+            _0x4c83xe(_0x4c83xf)
+        }
+    } catch (e) {
+        _0x4c83xe(_0x4c83xf)
+    }
+})({})
