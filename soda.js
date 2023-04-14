823d93d2d0e4a93c800cad514c93db2c35585449
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-14 16:07:22 +0800

    Update soda.js

--- soda.js
+++ soda.js
@@ -1,5 +1,5 @@
 let obj=JSON.parse($response.body);
 obj.data.objects.forEach(item => {
- item.imageUrl="https://raw.githubusercontent.com/jizhi0520/QX/main/warm%20water.png";;
+ item.imageUrl="https://raw.githubusercontent.com/jizhi0520/QX/main/warm_water.png";;
 });
 $done({body: JSON.stringify(obj)});
