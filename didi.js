dad9f4e06e08bf585525dcd2d64f247fb987b3f4
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-14 22:13:46 +0800

    Update didi.js

--- didi.js
+++ didi.js
@@ -1,2 +1,3 @@
 let obj=JSON.parse($response.body);
-body.data.sections = body.data.sections.filter(item => ["center_v2", "head_v2", "member", "core_function"].includes(item.sectionId));$done({body: JSON.stringify(obj)});
+obj.data.sections = obj.data.sections.filter(item => ["center_v2", "head_v2", "member", "core_function"].includes(item.sectionId));
+$done({body: JSON.stringify(obj)});
