9f6934b847b6774b72ed4ad283d25b6ead1c786e
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 13:44:42 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -1,4 +1,16 @@
-let obj=JSON.parse($response.body);
-delete obj.data.INDEX_SLOT_01 ;
-delete obj.data.INDEX_SLOT_02;
-$done({body: JSON.stringify(obj)});
+let obj = JSON.parse($response.body);
+
+delete obj.data.INDEX_SLOT_01;
+
+//删除所有👇滚动AD
+//delete obj.data.INDEX_TOP_BANNER.contents ;
+
+//保留一个👇
+delete obj.data.INDEX_TOP_BANNER.contents[1];
+//delete obj.data.INDEX_TOP_BANNER.contents[2];
+//delete obj.data.INDEX_TOP_BANNER.contents[3];
+//delete obj.data.INDEX_TOP_BANNER.contents[4];
+
+$done({
+    body: JSON.stringify(obj)
+});
