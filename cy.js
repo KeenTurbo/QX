809f60519e44766feadf092a3dbc2a3cf3de49db
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 21:19:53 +0800

    Update cy.js

--- cy.js
+++ cy.js
@@ -1,4 +1,3 @@
 let obj=JSON.parse($response.body);
-delete obj.data.INDEX_TOP_BANNER.contents[1].bubble ;
-delete obj.data.INDEX_TOP_BANNER.contents[1].figure ;
+delete obj.INDEX_TOP_BANNER.contents[1].bubble ;
 $done({body: JSON.stringify(obj)});
