b8f8b2c85fc3dd8a250f7f9972780977071c35be
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 16:20:05 +0800

    Update and rename xicha.js to heytea.js

--- heytea.js
+++ heytea.js
@@ -1,5 +1,5 @@
 let obj=JSON.parse($response.body);
 delete obj.data.ad ;
 delete obj.data.brands ;
-delete obj.data.brands.info.infos ;
+delete obj.data.info.infos ;
 $done({body: JSON.stringify(obj)});
