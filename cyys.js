9f25f83abda251af0bcd1671b2dd4c0d8f1fbd7d
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 14:46:06 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -6,15 +6,15 @@ delete obj.data.INDEX_SLOT_01;
 //delete obj.data.INDEX_TOP_BANNER.contents ;
 
 //换图
-obj.data.INDEX_TOP_BANNER.contents[0].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys.jpg";
+obj.data.INDEX_TOP_BANNER.contents[0].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys1.jpg";
 
-obj.data.INDEX_TOP_BANNER.contents[1].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys.jpg";
+obj.data.INDEX_TOP_BANNER.contents[1].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys1.jpg";
 
-obj.data.INDEX_TOP_BANNER.contents[2].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys.jpg";
+obj.data.INDEX_TOP_BANNER.contents[2].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys1.jpg";
 
-obj.data.INDEX_TOP_BANNER.contents[3].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys.jpg";
+obj.data.INDEX_TOP_BANNER.contents[3].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys1.jpg";
 
-obj.data.INDEX_TOP_BANNER.contents[4].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys.jpg";
+obj.data.INDEX_TOP_BANNER.contents[4].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyys1.jpg";
 
 //保留一个👇
 //delete obj.data.INDEX_TOP_BANNER.contents[1];
