084f8db6711c9a218ab5e4fbf8729f2c308534d3
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 02:50:19 +0800

    Update chayan.js

--- chayan.js
+++ chayan.js
@@ -1,5 +1,6 @@
 let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01;
+delete obj.data.INDEX_SLOT_02;
 let ads = obj.data.INDEX_TOP_BANNER.contents;
 ads.splice(1);
 delete ads[0].bubble;
