4a09d3282ee892597e0b44be5c67540d51cfb909
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 01:53:25 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -1,9 +1,5 @@
 let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01;
-delete obj.data.INDEX_SLOT_02;
-obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
-  delete item.bubble; 
-  delete item.figure; 
-  item.value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss.jpg";
-});
+obj.data?.INDEX_TOP_BANNER?.contents?.splice(1);
+obj.data.INDEX_TOP_BANNER.contents[0].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss.jpg";
 $done({body: JSON.stringify(obj)});
