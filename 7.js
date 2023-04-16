15004e93ec3ff608a97b997cf79a50b412b50f77
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 00:49:21 +0800

    Update 7.js

--- 7.js
+++ 7.js
@@ -2,6 +2,7 @@ let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01;
 obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
   delete item.bubble; 
+  delete item.figure; 
   item.value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss.jpg";
 });
 $done({body: JSON.stringify(obj)});
