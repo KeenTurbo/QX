ff7c71cbaf449021b3cd8faf803f54374fe2c6a7
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 00:54:25 +0800

    Delete cy1.js

--- cy1.js
+++
@@ -1,9 +0,0 @@
-let obj = JSON.parse($response.body);
-delete obj.data.INDEX_TOP_BANNER.contents[0].bubble;
-delete obj.data.INDEX_TOP_BANNER.contents[1].bubble;
-delete obj.data.INDEX_TOP_BANNER.contents[2].bubble;
-delete obj.data.INDEX_TOP_BANNER.contents[3].bubble;
-delete obj.data.INDEX_TOP_BANNER.contents[4].bubble;
-$done({
-    body: JSON.stringify(obj)
-});
