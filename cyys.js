bb483959629780dabfd664c41080d7544bfe1df4
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 01:34:24 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -1,5 +1,6 @@
 let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01;
+delete obj.data.INDEX_SLOT_02;
 obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
   delete item.bubble; 
   delete item.figure; 
