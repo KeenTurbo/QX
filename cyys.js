bde8cd8da5ad12205bd87fba7ee0bc3d8c2ba587
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 01:54:57 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -1,5 +1,6 @@
 let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01;
+delete obj.data.INDEX_SLOT_02;
 obj.data?.INDEX_TOP_BANNER?.contents?.splice(1);
 obj.data.INDEX_TOP_BANNER.contents[0].value = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss.jpg";
 $done({body: JSON.stringify(obj)});
