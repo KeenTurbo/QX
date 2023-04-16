6c4bc745a5fa23951cb7d2e0313c1df491be77d2
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 21:36:27 +0800

    Update cy1.js

--- cy1.js
+++ cy1.js
@@ -1,9 +1,9 @@
 let obj = JSON.parse($response.body);
-obj.data.INDEX_TOP_BANNER.contents[0].bubble = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss1.jpg";
-obj.data.INDEX_TOP_BANNER.contents[1].bubble = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss1.jpg";
-obj.data.INDEX_TOP_BANNER.contents[2].bubble = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss1.jpg";
-obj.data.INDEX_TOP_BANNER.contents[3].bubble = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss1.jpg";
-obj.data.INDEX_TOP_BANNER.contents[4].bubble = "https://raw.githubusercontent.com/jizhi0520/QX/main/cyyss1.jpg";
+delete obj.data.INDEX_TOP_BANNER.contents[0].bubble;
+delete obj.data.INDEX_TOP_BANNER.contents[1].bubble;
+delete obj.data.INDEX_TOP_BANNER.contents[2].bubble;
+delete obj.data.INDEX_TOP_BANNER.contents[3].bubble;
+delete obj.data.INDEX_TOP_BANNER.contents[4].bubble;
 $done({
     body: JSON.stringify(obj)
 });
