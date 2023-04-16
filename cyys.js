d1107653642e7e8c013243f3b4dccc9031658c32
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-16 13:04:23 +0800

    Update cyys.js

--- cyys.js
+++ cyys.js
@@ -1,3 +1,4 @@
 let obj=JSON.parse($response.body);
 delete obj.data.INDEX_SLOT_01 ;
+obj.data.INDEX_SLOT_02.contents.value = "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg" ;
 $done({body: JSON.stringify(obj)});
