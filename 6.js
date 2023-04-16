5ffc237db95d6ba7b3869203a05be9e703f98732
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 00:53:43 +0800

    Delete 6.js

--- 6.js
+++
@@ -1,6 +0,0 @@
-let obj=JSON.parse($response.body);
-obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
-  delete item.bubble; 
-});
-console.log(obj.data?.INDEX_TOP_BANNER);
-$done({body: JSON.stringify(obj)});
