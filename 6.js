51251171454da4b0ad885f2a9d5a78289a645270
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 00:40:34 +0800

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,6 +1,6 @@
 let obj=JSON.parse($response.body);
-obj.data?.INDEX_TOP_BANNER?.contents.forEach(item => {
+obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
   delete item.bubble; 
 });
-console.log(obj);
+console.log(obj.data?.INDEX_TOP_BANNER);
 $done({body: JSON.stringify(obj)});
