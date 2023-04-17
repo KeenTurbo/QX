c7f8effaac3cd94ec03442356b9825a77d094854
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 14:58:10 +0800

    Update ems.js

--- ems.js
+++ ems.js
@@ -1,3 +1,5 @@
-let obj = JSON.parse($response.body);
-obj.info.moduleJson = JSON.stringify(JSON.parse(obj.info.moduleJson).filter(item => !item.moduleName.includes("广告")));
+let obj=JSON.parse($response.body);
+let ads = ["obj.data.index.sknComponents","obj.data.mall_index.sknComponents"];
+ads[0] = ads[0].filter(item => ["广告"].includes(item.title));
+ads[1] = ads[1].filter(item => ["广告"].includes(item.title));
 $done({body: JSON.stringify(obj)});
