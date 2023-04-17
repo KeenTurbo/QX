8b6f70c7adcd81a4af7092e33e755bbe632247d7
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-18 07:51:20 +0800

    Update hxz.js

--- hxz.js
+++ hxz.js
@@ -1,4 +1,4 @@
 let obj=JSON.parse($response.body);
+delete obj.data.p_webapp_home_preferential_banner.close_tracks ;
 delete obj.data.p_webapp_home_preferential_banner ;
-delete obj.data.p_webapp_home_preferential_banner
 $done({body: JSON.stringify(obj)});
