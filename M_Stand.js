7f3b8044c5714f9137e8eba1416c4714d220e2b0
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-18 01:51:13 +0800

    Update M_Stand.js

--- M_Stand.js
+++ M_Stand.js
@@ -1,5 +1,6 @@
 let obj=JSON.parse($response.body);
 delete obj.data.homeNewsAdv ;
+delete obj.data.homePickupAdv ;
 delete obj.data.homeBannerAdv ;
 delete obj.data.homeDialogAdv ;
 delete obj.data.homeTopAdv ;
