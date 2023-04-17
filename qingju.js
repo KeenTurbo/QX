a318a1c2694fddaf41e54ebd858afc39c0f40661
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 10:48:01 +0800

    Update qingju.js

--- qingju.js
+++ qingju.js
@@ -1,4 +1,4 @@
 let obj=JSON.parse($response.body);
 delete obj.data.bannerInfoConfig ;
-delete obj.data.nextRightDetailVO ;
+delete obj.data.memberBaseInfoEnterResult ;
 $done({body: JSON.stringify(obj)});
