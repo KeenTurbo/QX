8d5d19e57fe6d7db82385b37b902257e108a296f
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-18 02:36:19 +0800

    Delete 009.js

--- 009.js
+++
@@ -1,9 +0,0 @@
-let obj=JSON.parse($response.body);
-delete obj.data.homeDineInAdv ;
-delete obj.data.homePickupAdv ;
-delete obj.data.nearbyShopInfo ;
-delete obj.data.homeEventThemesAdv ;
-delete obj.data.homeCouponAdv ;
-delete obj.data.homeCompanyAdv ;
-delete obj.data.homeDeliveryAdv ;
-$done({body: JSON.stringify(obj)});
