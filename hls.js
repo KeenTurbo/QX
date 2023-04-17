9fe777de40b4326addfa0f323769207125405fb6
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-17 15:14:18 +0800

    Update hls.js

--- hls.js
+++ hls.js
@@ -1,5 +1,4 @@
 let obj=JSON.parse($response.body);
-let ads = ["obj.data.index.sknComponents","obj.data.mall_index.sknComponents"];
-ads[0] = ads[0].filter(item => ["广告"].includes(item.title));
-ads[1] = ads[1].filter(item => ["广告"].includes(item.title));
+obj.data.index.sknComponents = obj.data.index.sknComponents.filter(item => ["广告"].includes(item.title));
+obj.data.mall_index.sknComponents = obj.data.mall_index.sknComponents.filter(item => ["广告"].includes(item.title));
 $done({body: JSON.stringify(obj)});
