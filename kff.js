2480865af6a7e04ce944367c941b959475b6ecce
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-18 14:29:32 +0800

    Update kff.js

--- kff.js
+++ kff.js
@@ -1,3 +1,5 @@
 let obj = JSON.parse($response.body);
 obj.data.config = JSON.stringify(JSON.parse(obj.data.config).filter(item => !item.title.includes("会员画布")));
+obj.data.config = JSON.stringify(JSON.parse(obj.data.config).filter(item => !item.title.includes("图片")));
+obj.data.config = JSON.stringify(JSON.parse(obj.data.config).filter(item => !item.title.includes("图片热点")));
 $done({body: JSON.stringify(obj)});
