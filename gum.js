8aa2ccb340dca8ec7eea772b84c6c94be6162bb2
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-19 15:31:48 +0800

    Update gum.js

--- gum.js
+++ gum.js
@@ -1,7 +1,3 @@
 let obj=JSON.parse($response.body);
 delete obj.data.homeCommunityEntryBg ;
-delete obj.data.inviteActivityLandingGoodImg ;
-delete obj.data.defaultPosterPic ;
-delete obj.data.customBannerForCity.imgUrl ;
-delete obj.data.indexShareConfig ;
 $done({body: JSON.stringify(obj)});
