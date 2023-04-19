82099f4a33f31df780a576d52d74f79ff01253ac
0520 <72007390+jizhi0520@users.noreply.github.com>
2023-04-19 15:16:37 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,3 +1,3 @@
 let obj = JSON.parse($response.body);
-obj.data.actionPointList = JSON.stringify(JSON.parse(obj.data.actionPointList).filter(item => !item.pointCode.includes("homeNews")));
+obj.data.actionPointList= JSON.stringify(JSON.parse(obj.data.actionPointList).filter(item => !item.pointName.includes("新品公告")));
 $done({body: JSON.stringify(obj)});
