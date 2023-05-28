e05893aac9439a87aee3bba6c57d0f4472470e14
6335363 <72007390+jizhi0520@users.noreply.github.com>
2023-05-28 16:15:30 +0800

    Create iT.js

---
+++ iT.js
@@ -0,0 +1,23 @@
+/******************************
+
+脚本功能：iTanslate 订阅
+下载地址：https://is.gd/4QGRR6
+特别说明：如不能解锁，必须试用一次即可！
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+https://ssl-api.itranslateapp.com/accounts/v4/subscriptions/verify/ios url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main//iT.js
+
+[mitm] 
+
+hostname = ssl-api.itranslateapp.com
+
+*******************************/
+
+let obj = 
+{"licenses":[{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":32493834549000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"},{"bundle_id":"com.outerspaceapps.itranslate","expires_date_ms":32493834549000,"is_trial_period":true,"original_transaction_id":"160000714859958","product_id":"com.itranslate.pro.monthly","transaction_id":"160000714859958"}]}
+}
+$done({body: JSON.stringify(obj)});
