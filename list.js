094757e590392797bc2043ae5ab7f346d50622b0
6335363 <72007390+jizhi0520@users.noreply.github.com>
2023-06-02 20:10:10 +0800

    Create list.js

---
+++ list.js
@@ -0,0 +1,4 @@
+hostname = app.bokuigo.cn
+
+https://app.bokuigo.cn:9004/daily/appLoginAction url response-body word_count_limit":240 response-body word_count_limit":9999
+https://app.bokuigo.cn:9004/daily/appMeAction url response-body word_count_limit":240 response-body word_count_limit":9999
