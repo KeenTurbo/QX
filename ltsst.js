566e5f14a0e4c6bf6e9f507dd9b7df05b12f43ce
6335363 <72007390+jizhi0520@users.noreply.github.com>
2023-06-04 14:37:42 +0800

    Create ltsst.js

---
+++ ltsst.js
@@ -0,0 +1,43 @@
+功能：旅途随身听 解锁内购
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+*******************************
+
+[rewrite_local]
+# > 旅途随身听 解锁内购
+^httpshttps://www.1314zhilv.com/ltsstnew/user/getInfo url script-response-body https://raw.githubusercontent.com/jizhi0520/QX/main/ltsst.js
+
+
+[mitm] 
+hostname=www.1314zhilv.com
+
+*******************************/
+
+
+let body = {
+  "success" : true,
+  "message" : "success",
+  "state" : 200,
+  "content" : {
+    "headUrl" : null,
+    "isMountSwitch" : 0,
+    "isAnchor" : 1,
+    "isSYGuidePopUp" : 1,
+    "isShowAppletWX" : 0,
+    "isDistributor" : 0,
+    "isShowGrey" : 0,
+    "levDesc" : "尊贵的",
+    "totalCashBalance" : 0,
+    "userName" : "",
+    "userId" : "",
+    "phoneNum" : "",
+    "identityType" : 1,
+    "cashBalance" : 0,
+    "isShowBroadcast" : 0,
+    "isShowShareBtn" : 0,
+    "isShowAppletDY" : 0,
+    "isActivate" : 1
+  }
+}
+
+$done({ body: JSON.stringify(body) });
